26f32ab3c796eb5e92a2a4fe37d791f593b15943
Anthony Howell <anthonyamh@gmail.com>
2021-01-23 13:57:31 -0600

    Created/implemented outputField, increment button, and decrement button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 50;
+        TextView t = (TextView) findViewById(R.id.outputField);
+        t.setText(counter.toString());
+    }
+
+    public void onClickInputButtonInc(View v) {
+        TextView t = (TextView) findViewById(R.id.outputField);
+        counter++;
+        t.setText(counter.toString());
+    }
+
+    public void onClickInputButtonDec(View v) {
+        TextView t = (TextView) findViewById(R.id.outputField);
+        counter--;
+        t.setText(counter.toString());
     }
 }
